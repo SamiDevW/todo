18a3641f4590f4a3869890e97e6f8cbee67d9be7
SamiDevW <SamiDevW@users.noreply.github.com>
2024-06-06 01:35:15 +0200

    none

--- verticalmode.js
+++ verticalmode.js
@@ -15,12 +15,15 @@ btnVertical.addEventListener('click', () => {
             x.style.flexDirection = 'row'
             x.style.gap = "20px"
             x.style.width = "fit-content"
+            x.style.maxWidth = "none"
         })
     }
     else if (window.getComputedStyle(savedEl).flexDirection === 'column') {
         savedEl.style.flexDirection = 'row';
         savedElements.forEach(x => {
             x.style.flexDirection = 'column'
+            x.style.maxWidth = "600px"
+
 
         })
     }
